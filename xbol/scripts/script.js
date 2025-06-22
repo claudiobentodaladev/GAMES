a51733bdd0715ed6f67b72a5edcee4544c869def
claudiobentodala <programmer.claudio@gmail.com>
2025-06-22 11:44:55 +0100

    done: logic changer and got the code clean

--- xbol/scripts/script.js
+++ xbol/scripts/script.js
@@ -9,6 +9,7 @@ const box = [
     window.document.querySelector('div#b8'),
     window.document.querySelector('div#b9')
 ]
+var box_ckecker = [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined]
 
 function x_o(player) {
     if (player) {
@@ -24,46 +25,45 @@ function color_x_o(player) {
         return 'blue'
     }
 }
+function x_o_logic(index,bool) {
+    if (box_ckecker[index] == undefined) {
+        box[index].innerText = x_o(bool)
+        box[index].style.color = color_x_o(bool)
+        box_ckecker[index] = bool
+        box[index].style.cursor = 'not-allowed'
+    }
+}
 
 var state = true // O(true) | X(false)
 
 function box_alert(boxnumber) {
     switch(boxnumber){
         case 1 : 
-            box[0].innerText = x_o(state)
-            box[0].style.color = color_x_o(state)
+            x_o_logic(0,state)
         break
         case 2 : 
-            box[1].innerText = x_o(state)
-            box[1].style.color = color_x_o(state)
+            x_o_logic(1,state)
         break
-        case 3 : 
-            box[2].innerText = x_o(state)
-            box[2].style.color = color_x_o(state)
+        case 3 :
+            x_o_logic(2,state) 
         break
-        case 4 : 
-            box[3].innerText = x_o(state)
-            box[3].style.color = color_x_o(state)
+        case 4 :
+            x_o_logic(3,state) 
         break
-        case 5 : 
-            box[4].innerText = x_o(state)
-            box[4].style.color = color_x_o(state)
+        case 5 :
+            x_o_logic(4,state) 
         break
-        case 6 : 
-            box[5].innerText = x_o(state)
-            box[5].style.color = color_x_o(state)
+        case 6 :
+            x_o_logic(5,state) 
         break
-        case 7 : 
-            box[6].innerText = x_o(state)
-            box[6].style.color = color_x_o(state)
+        case 7 :
+            x_o_logic(6,state) 
         break
-        case 8 : 
-            box[7].innerText = x_o(state)
-            box[7].style.color = color_x_o(state)
+        case 8 :
+            x_o_logic(7,state) 
         break
-        case 9 : 
-            box[8].innerText = x_o(state)
-            box[8].style.color = color_x_o(state)
+        case 9 :
+            x_o_logic(8,state) 
         break
     }
     state = !state
